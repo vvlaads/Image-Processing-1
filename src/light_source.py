4e6d2fb35c3c5b73eda1ff1af5e377b9c1f5ff0d
vvlaads <vvlaads@gmail.com>
2026-03-02 01:14:05 +0300

    Added documentation for light_source

--- src/light_source.py
+++ src/light_source.py
@@ -1,5 +1,14 @@
 class LightSource:
+    """Источник света с заданным положением, направлением и силой излучения"""
+
     def __init__(self, point, illumination, n):
+        """
+        Конструктор источника света.
+
+        :param point: Точка расположения источника света.
+        :param illumination: Сила излучения вдоль оси источника.
+        :param n: Нормаль (ось) источника света — направление максимального излучения.
+        """
         self.point = point
         self.illumination = illumination
         self.n = n
