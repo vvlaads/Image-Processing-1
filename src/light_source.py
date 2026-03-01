d0d3d01c09ab516735a9d779b57f17ce1ea3c4ef
vvlaads <vvlaads@gmail.com>
2026-03-02 01:32:40 +0300

    Fixed naming: we need to use intensity (not illumination)

--- src/light_source.py
+++ src/light_source.py
@@ -1,14 +1,14 @@
 class LightSource:
     """Источник света с заданным положением, направлением и силой излучения"""
 
-    def __init__(self, point, illumination, n):
+    def __init__(self, point, intensity, n):
         """
         Конструктор источника света.
 
         :param point: Точка расположения источника света.
-        :param illumination: Сила излучения вдоль оси источника.
+        :param intensity: Сила излучения вдоль оси источника.
         :param n: Нормаль (ось) источника света — направление максимального излучения.
         """
         self.point = point
-        self.illumination = illumination
+        self.intensity = intensity
         self.n = n
